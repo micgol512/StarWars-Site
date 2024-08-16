320d9d23e8171fefb16161ac724e7718b06fbde6
Michał Gołębiowski <m.golebiowski.fm@gmail.com>
2024-08-16 15:23:57 +0200

    FinalVersion after Feedback

--- scripts.js
+++ scripts.js
@@ -1,21 +1,112 @@
 import { rowData } from "./data.js";
 
-function formatDate(date) {
-  date = new Date(date);
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
+const $body = document.body;
+//zostałem przy tym rozwiązaniu bo tablica bedzie prechowywała zawsze max 5 znaków
+//gdzie string jak nidy nie wpiszemy słowa będzie się powiększał
+const keyCatcher = ["."];
+
+const startPage = () => {
+  createHeader();
+  hiddenPowerSound();
+  createMainBtns();
+  createStartLogo();
+};
+const createHeader = () => {
+  const header = document.createElement("header");
+  const radioWrapper = document.createElement("div");
+  const label = document.createElement("label");
+
+  header.innerHTML = `<div>Wpisz '<strong>YODA</strong>' albo '<strong>VADER</strong>' by skorzystać z tajnej mocy!!!</div>`;
+
+  radioWrapper.id = "radio-wrapper";
+  label.textContent = "Wybierz moc strony:";
+  label.htmlFor = "selectSite";
+  radioWrapper.append(label);
+  for (let i = 1; i <= 3; i++) {
+    const radio = document.createElement("input");
+    radio.id = `site-${i}`;
+    radio.type = "radio";
+    radio.name = "selectSite";
+    radio.className = "radioSite";
+    i === 1 ? (radio.checked = true) : null;
+    radioWrapper.append(radio);
+    radio.addEventListener("click", () => {
+      setColor(i);
+    });
+  }
+
+  header.append(radioWrapper);
+  $body.append(header);
+};
+const hiddenPowerSound = () => {
+  const keyVader = "vader";
+  const keyYoda = "yoda";
+  const vaderSound = new Audio("./media/audio/vader.mp3");
+  const yodaSound = new Audio("./media/audio/yoda.mp3");
+  document.addEventListener("keypress", ({ key }) => {
+    keyCatcher.push(key.toLowerCase());
+    if (keyCatcher.length > keyVader.length) {
+      keyCatcher.shift();
+    }
+    if (keyCatcher.join("").slice(1) === keyYoda) {
+      vaderSound.pause();
+      vaderSound.currentTime = 0;
+      yodaSound.play();
+    } else if (keyCatcher.join("") === keyVader) {
+      yodaSound.pause();
+      yodaSound.currentTime = 0;
+      vaderSound.play();
+    }
+  });
+};
+const createMainBtns = () => {
+  const menuBtnWrapper = document.createElement("div");
+  const menuBtnNames = Object.keys(rowData);
+
+  menuBtnWrapper.className = "buttons wrapper";
+  $body.append(menuBtnWrapper);
+
+  menuBtnNames.forEach((btnName, index) => {
+    const btn = document.createElement("button");
+    menuBtnWrapper.append(btn);
+    btn.id = `menuBtn-${index}`;
+    btn.className = "menuBtns";
+    btn.innerText = btnName.toUpperCase();
+    btn.addEventListener("click", () => {
+      removeStartLogo();
+      for (let i = 0; i < menuBtnNames.length; i++) {
+        document.getElementById(`menuBtn-${i}`).className = "menuBtns";
+      }
+      btn.className = "menuBtns active";
+      createTable(btnName);
+    });
+  });
+};
+const createStartLogo = () => {
+  const logo = document.createElement("img");
+  logo.src = "./media/images/logo.png";
+  logo.id = "logo";
+  $body.append(logo);
+};
+const removeStartLogo = () => {
+  if (document.getElementById("logo")) document.getElementById("logo").remove();
+};
+const formatDate = (date) => {
+  const formatedDate = new Date(date);
+  const year = formatedDate.getFullYear();
+  const month = String(formatedDate.getMonth() + 1).padStart(2, "0");
+  const day = String(formatedDate.getDate()).padStart(2, "0");
   return `${day}-${month}-${year}`;
-}
-function createTable(key) {
+};
+const createTable = (key) => {
   if (document.getElementById("wrapper-content")) {
     document.getElementById("wrapper-content").remove();
     document.getElementById("wrapper-search").remove();
   }
   const searcher = document.createElement("div");
   const contenter = document.createElement("div");
-  searcher.setAttribute("id", "wrapper-search");
-  searcher.setAttribute("class", "wrapper searcher");
+  searcher.id = "wrapper-search";
+  searcher.className = "wrapper searcher";
   searcher.innerHTML = `
   <div class="wrapper-searcher">
   <label for="input-search-index">Search by index:</label>
@@ -25,11 +116,11 @@ function createTable(key) {
   <label for="input-search-text">Search by text:</label>
   <input id="input-search-text" class="searcher-by" />
   </div>`;
-  contenter.setAttribute("id", "wrapper-content");
-  contenter.setAttribute("class", "wrapper content");
+  contenter.id = "wrapper-content";
+  contenter.className = "wrapper content";
   $body.append(searcher, contenter);
   const tableContent = document.createElement("table");
-  tableContent.setAttribute("id", "table-content");
+  tableContent.id = "table-content";
   tableContent.innerHTML = `<thead id="table-head">
       </thead><tbody id="table-body"></tbody>`;
   contenter.append(tableContent);
@@ -37,7 +128,7 @@ function createTable(key) {
   createData();
 
   const pagesWrapper = document.createElement("div");
-  pagesWrapper.setAttribute("id", "wrapper-page-changer");
+  pagesWrapper.id = "wrapper-page-changer";
   pagesWrapper.innerHTML = `
   <button class="btn-pages" disabled>❰</button>
     <input type="number" id="curr-page" placeholder = "1" value="1"/>
@@ -70,14 +161,14 @@ function createTable(key) {
           alert("Coś jest źle!!");
           break;
       }
-      paginacja();
+      pagination();
     });
   });
-  paginacja();
+  pagination();
   const tBody = Array.from(document.querySelectorAll("#table-body tr"));
   let searchTempText = "name";
   if (key === "films") searchTempText = "title";
-  searchByTxt.setAttribute("placeholder", `Search by ${searchTempText}.`);
+  searchByTxt.placeholder = `Search by ${searchTempText}.`;
   ///////////////////////////////////////////////////////////////////////////////////////
   searchById.addEventListener("input", () => {
     searchByTxt.value = "";
@@ -98,7 +189,7 @@ function createTable(key) {
       parseInt(currPage.value) > parseInt(allPages.innerText)
     )
       currPage.value = "";
-    paginacja();
+    pagination();
   });
   currPage.addEventListener("blur", () => {
     if (currPage.value === "") {
@@ -111,26 +202,29 @@ function createTable(key) {
       document.querySelectorAll("#table-body tr:not([class])")
     ).findIndex((element) => element.style.display !== "none");
     currPage.value = Math.floor(firstRow / pages.value) + 1;
-    paginacja();
+    pagination();
   });
-}
-
-function checkBoxAddListener() {
+};
+//oddzielna funkcja bo jak tworzyłem tam gdzie przyciski to odwoływało się tylko do ostatnio
+//zaznaczonego checkboxa tylko
+const checkBoxAddListener = () => {
   let checkBoxTab = Array.from(document.querySelectorAll(".checkBoxSelect"));
   let btnDelChecked = null;
   checkBoxTab.forEach((check, index) => {
-    function removeRowWithIndex() {
+    //jak definiuje tą funkcję poza i odnoszę się to przy odznaczeniu checkboxa
+    //nie usówa mi Listenera czyli usówa nawet odznaczone (ale zaznaczone kiedyś i odznaczone)
+    const removeRowWithIndex = () => {
       document.getElementById(`row-${index}`).remove();
       btnDelChecked.remove();
-      paginacja();
-    }
+      pagination();
+    };
     check.addEventListener("change", () => {
       checkBoxTab = Array.from(document.querySelectorAll(".checkBoxSelect"));
       let isSomeChecked = checkBoxTab.some((item) => item.checked);
       if (!document.getElementById("btnDelChecked")) {
         btnDelChecked = document.createElement("button");
         btnDelChecked.textContent = "Remove all";
-        btnDelChecked.setAttribute("id", "btnDelChecked");
+        btnDelChecked.id = "btnDelChecked";
         document.getElementById("wrapper-search").append(btnDelChecked);
       }
       if (!isSomeChecked) {
@@ -143,89 +237,104 @@ function checkBoxAddListener() {
       }
     });
   });
-}
-function createActionsBtns(parent, index) {
+};
+const createActionsBtns = (parent, index) => {
   const activeBtn = document.querySelector(".active").innerText.toLowerCase();
-  const editBtn = document.createElement("button");
-  editBtn.textContent = "✙";
-  editBtn.setAttribute("class", "showBtn");
+  const showBtn = document.createElement("button");
+  showBtn.textContent = "✙";
+  showBtn.className = "showBtn";
   const deleteBtn = document.createElement("button");
-  deleteBtn.setAttribute("class", "deleteBtn");
+  deleteBtn.className = "deleteBtn";
   // deleteBtn.textContent = "✘";
   const checkBox = document.createElement("input");
-  checkBox.setAttribute("class", "checkBoxSelect");
-  checkBox.setAttribute("type", "checkbox");
+  checkBox.className = "checkBoxSelect";
+  checkBox.type = "checkbox";
   const $td = document.createElement("td");
-  $td.setAttribute("class", "actions-buttons");
-  $td.append(editBtn, deleteBtn, checkBox);
+  $td.className = "actions-buttons";
+  $td.append(showBtn, deleteBtn, checkBox);
   parent.append($td);
 
-  editBtn.addEventListener("click", () => {
+  showBtn.addEventListener("click", () => {
     createInfo(activeBtn, index);
-    document
-      .getElementById("closeInfoBtn")
-      .addEventListener("click", function () {
-        document.getElementById("showInfoBox").remove();
-      });
+    document.getElementById("closeInfoBtn").addEventListener("click", () => {
+      document.getElementById("showInfoBox").remove();
+      $body.style.overflow = "auto";
+    });
   });
   deleteBtn.addEventListener("click", () => {
     parent.remove();
-    paginacja();
+    pagination();
   });
-}
-function setColor(select) {
+};
+const setColor = (select) => {
   switch (select) {
     case 1:
-      $root.documentElement.style.setProperty("--mainColor", "rgb(0, 255, 21)");
-      $root.documentElement.style.setProperty(
+      document.documentElement.style.setProperty(
+        "--mainColor",
+        "rgb(0, 255, 21)"
+      );
+      document.documentElement.style.setProperty(
         "--btnBgHoverColor",
         "rgba(0, 255, 0, 0.425)"
       );
-      $root.documentElement.style.setProperty(
+      document.documentElement.style.setProperty(
         "--btnHoverColor",
         "rgb(241, 206, 206)"
       );
-      $root.documentElement.style.setProperty("--infoColor", " rgb(0, 70, 0)");
+      document.documentElement.style.setProperty(
+        "--infoColor",
+        " rgb(0, 70, 0)"
+      );
       break;
     case 2:
-      $root.documentElement.style.setProperty("--mainColor", "rgb(255, 0, 0)");
-      $root.documentElement.style.setProperty(
+      document.documentElement.style.setProperty(
+        "--mainColor",
+        "rgb(255, 0, 0)"
+      );
+      document.documentElement.style.setProperty(
         "--btnBgHoverColor",
         "rgba(255, 0, 0, 0.425)"
       );
-      $root.documentElement.style.setProperty(
+      document.documentElement.style.setProperty(
         "--btnHoverColor",
         "rgb(241, 206, 206)"
       );
-      $root.documentElement.style.setProperty("--infoColor", " rgb(70, 0, 0)");
+      document.documentElement.style.setProperty(
+        "--infoColor",
+        " rgb(70, 0, 0)"
+      );
       break;
     case 3:
-      $root.documentElement.style.setProperty(
+      document.documentElement.style.setProperty(
         "--mainColor",
         "rgb(0, 225, 255)"
       );
-      $root.documentElement.style.setProperty(
+      document.documentElement.style.setProperty(
         "--btnBgHoverColor",
         "rgba(0, 225, 255, 0.425)"
       );
-      $root.documentElement.style.setProperty(
+      document.documentElement.style.setProperty(
         "--btnHoverColor",
         "rgb(206, 241, 241)"
       );
-      $root.documentElement.style.setProperty("--infoColor", "rgb(0, 0, 70)");
+      document.documentElement.style.setProperty(
+        "--infoColor",
+        "rgb(0, 0, 70)"
+      );
       break;
 
     default:
       break;
   }
-}
-function createInfo(btnName, index) {
+};
+const createInfo = (btnName, index) => {
   if (document.getElementById("showInfoBox")) {
     document.getElementById("showInfoBox").remove();
   }
   const infoRow = rowData[btnName][index];
   const windowInfo = document.createElement("div");
-  windowInfo.setAttribute("id", "showInfoBox");
+  $body.style.overflow = "hidden";
+  windowInfo.id = "showInfoBox";
   windowInfo.innerHTML = `<div id="info-wrapper">
     <div class="info-title"><span><strong> ${
       infoRow.name || infoRow.title
@@ -269,8 +378,8 @@ function createInfo(btnName, index) {
     });
     infoContent.innerHTML += "</ul>";
   }
-}
-function createData() {
+};
+const createData = () => {
   const activeBtn = document.querySelector(".active").innerText.toLowerCase();
   if (document.getElementById("nothing-to-show"))
     document.getElementById("nothing-to-show").remove();
@@ -319,7 +428,7 @@ function createData() {
   const tbody = document.getElementById("table-body");
   content.forEach((element, index) => {
     const $tr = document.createElement("tr");
-    $tr.setAttribute("id", `row-${index}`);
+    $tr.id = `row-${index}`;
     const $td1 = document.createElement("td");
     const $td2 = document.createElement("td");
     const $td3 = document.createElement("td");
@@ -335,35 +444,37 @@ function createData() {
     createActionsBtns($tr, index);
   });
   checkBoxAddListener();
-}
-function checkButtonPages() {
+};
+const checkButtonPages = () => {
   const btnChange = document.querySelectorAll(".btn-pages");
   const allPages = document.getElementById("all-pages");
   const currPage = document.getElementById("curr-page");
   if (allPages.innerText === "1") {
-    btnChange[0].setAttribute("disabled", "");
-    btnChange[1].setAttribute("disabled", "");
+    btnChange[0].disabled = true;
+    btnChange[1].disabled = true;
   } else {
+    //musi sprawdzać czy pusty string bo jak nie to można pójść np na stronę 2
+    //wejść poza zakres tj ustawić stronę na 1 i wtedy przyciskiem wejść na minusowe strony
     if (currPage.value !== "") {
-      btnChange[0].removeAttribute("disabled");
-      btnChange[1].removeAttribute("disabled");
+      btnChange[0].disabled = false;
+      btnChange[1].disabled = false;
       if (parseInt(currPage.value) === 1) {
-        btnChange[0].setAttribute("disabled", "");
+        btnChange[0].disabled = true;
       } else {
-        btnChange[0].removeAttribute("disabled");
+        btnChange[0].disabled = false;
       }
       if (parseInt(currPage.value) === parseInt(allPages.innerText)) {
-        btnChange[1].setAttribute("disabled", "");
+        btnChange[1].disabled = true;
       } else {
-        btnChange[1].removeAttribute("disabled");
+        btnChange[1].disabled = false;
       }
     } else {
-      btnChange[0].setAttribute("disabled", "");
-      btnChange[1].setAttribute("disabled", "");
+      btnChange[0].disabled = true;
+      btnChange[1].disabled = true;
     }
   }
-}
-function searchByIndex(search = "") {
+};
+const searchByIndex = (search = "") => {
   const tBodyRows = Array.from(document.querySelectorAll("#table-body tr"));
   tBodyRows.forEach((element) => {
     element.removeAttribute("class");
@@ -380,11 +491,11 @@ function searchByIndex(search = "") {
       element.querySelector("td").innerText.toLowerCase() !==
         search.toLowerCase()
     )
-      element.setAttribute("class", "disabled");
+      element.className = "disabled";
   });
-  paginacja();
-}
-function searchByText(text = "") {
+  pagination();
+};
+const searchByText = (text = "") => {
   const tBodyRows = Array.from(document.querySelectorAll("#table-body tr"));
   tBodyRows.forEach((element) => {
     element.removeAttribute("class");
@@ -394,11 +505,11 @@ function searchByText(text = "") {
         .innerText.toLowerCase()
         .includes(text.toLowerCase())
     )
-      element.setAttribute("class", "disabled");
+      element.className = "disabled";
   });
-  paginacja();
-}
-function paginacja() {
+  pagination();
+};
+const pagination = () => {
   const tBody = Array.from(document.querySelectorAll("#table-body tr"));
   const tBodyClassless = Array.from(
     document.querySelectorAll("#table-body tr:not([class])")
@@ -416,14 +527,14 @@ function paginacja() {
     document.getElementById("nothing-to-show").remove();
   if (currPageValue > 1 && currPageValue > allPages.innerText) {
     document.getElementById("curr-page").value--;
-    paginacja();
+    pagination();
   } else {
     if (
       tBodyClassless.length === 0 &&
       document.getElementById("nothing-to-show") === null
     ) {
       const nothingToShow = document.createElement("tr");
-      nothingToShow.setAttribute("id", "nothing-to-show");
+      nothingToShow.id = "nothing-to-show";
       nothingToShow.innerHTML += `<td></td><td>"Brak elementów do wyświetlenia"</td>`;
       document.getElementById("table-body").append(nothingToShow);
     } else {
@@ -438,75 +549,9 @@ function paginacja() {
     checkButtonPages();
   }
   const searchById = document.getElementById("input-search-index");
-  searchById.setAttribute(
-    "placeholder",
-    `${tBody.length === 0 ? "0" : "1"} from ${tBody.length}`
-  );
-}
+  searchById.placeholder = `${tBody.length === 0 ? "0" : "1"} from ${
+    tBody.length
+  }`;
+};
 
-//////////////////  START ////////////////////
-
-const menuBtnNames = Object.keys(rowData);
-const $body = document.body;
-const $root = document;
-const header = document.createElement("header");
-header.innerHTML = `<div>Wpisz '<strong>YODA</strong>' albo '<strong>VADER</strong>' by skorzystać z tajnej mocy!!!</div>`;
-const radioWrapper = document.createElement("div");
-radioWrapper.setAttribute("id", "radio-wrapper");
-header.append(radioWrapper);
-const label = document.createElement("label");
-label.textContent = "Wybierz moc strony:";
-label.setAttribute("for", "selectSite");
-radioWrapper.append(label);
-//color site
-for (let i = 1; i <= 3; i++) {
-  const radio = document.createElement("input");
-  radio.setAttribute("id", `site-${i}`);
-  radio.setAttribute("type", "radio");
-  radio.setAttribute("name", "selectSite");
-  radio.setAttribute("class", "radioSite");
-  i === 1 ? radio.setAttribute("checked", true) : null;
-  radioWrapper.append(radio);
-  radio.addEventListener("click", () => {
-    setColor(i);
-  });
-}
-// yoda lub vader sound
-const keyVader = "vader";
-const keyYoda = "yoda";
-let keyCatcher = ["."];
-const vaderSound = new Audio("./media/audio/vader.mp3");
-const yodaSound = new Audio("./media/audio/yoda.mp3");
-document.addEventListener("keypress", ({ key }) => {
-  keyCatcher.push(key.toLowerCase());
-  if (keyCatcher.length > keyVader.length) {
-    keyCatcher.shift();
-  }
-  if (keyCatcher.join("").slice(1) === keyYoda) {
-    yodaSound.play();
-  } else if (keyCatcher.join("") === keyVader) {
-    vaderSound.play();
-  }
-});
-//przyciski menu + start strony
-const menuBtnWrapper = document.createElement("div");
-menuBtnWrapper.setAttribute("class", "buttons wrapper");
-const logo = document.createElement("img");
-logo.src = "./media/images/logo.png";
-logo.setAttribute("id", "logo");
-$body.append(header, menuBtnWrapper, logo);
-menuBtnNames.forEach((btnName, index) => {
-  const btn = document.createElement("button");
-  menuBtnWrapper.append(btn);
-  btn.setAttribute("id", `menuBtn-${index}`);
-  btn.setAttribute("class", `menuBtns`);
-  btn.innerText = btnName.toUpperCase();
-  btn.addEventListener("click", () => {
-    logo.remove();
-    for (let i = 0; i < menuBtnNames.length; i++) {
-      document.getElementById(`menuBtn-${i}`).setAttribute("class", `menuBtns`);
-    }
-    btn.setAttribute("class", `menuBtns active`);
-    createTable(btnName);
-  });
-});
+startPage();
